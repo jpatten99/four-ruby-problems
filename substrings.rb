8bcdcd32f2f7c877923904b4b207a97369300f33
Joshua <joshuagpatten@gmail.com>
2022-03-17 15:09:06 -0400

    add case insensitivity

--- substrings.rb
+++ substrings.rb
@@ -2,12 +2,10 @@ def substrings(inputString, dictionary)
   outputHash = Hash.new
   dictionary.each do |word|
     if inputString.include? word
-      #puts "hi"
-      puts inputString.scan(word).length
-      outputHash[word] = inputString.scan(word).length
+      outputHash[word.downcase] = inputString.downcase.scan(word.downcase).length
     end
   end
   puts outputHash
 end
 
-substrings("Hohohello", ["hello", "goodybe", "ho"])
+substrings("Hohohello", ["hello", "goodybe", "ho", "h", "H"])
